5897f5a5fabba6bdf64f7af03d9575003ef35400
Ben199810 <ben29774583@gmail.com>
2025-02-25 10:52:08 +0800

    feat: 整理 aws 專案使用的 shell script

---
+++ aws/aws_sso_login.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 取得執行腳本當前目錄
+DIR="$(dirname $0)"
+
+# 字體顏色
+source ${DIR}/modules/colors.sh
+
+accounts_list=($(aws configure list-profiles))
+
+PS3="Select AWS Account: "
+select selected_account in "${accounts_list[@]}"; do
+    case $selected_account in
+      *)
+        if [[ -n "$selected_account" ]]; then
+          echo -e "${BLUE}Selected AWS Account: $selected_account${NC}"
+          aws sso login --profile $selected_account
+          echo -e "${GREEN}SSO Success login: $selected_account${NC}"
+        else
+          echo -e "${RED}Invalid selection.${NC}"
+        fi
+        break
+        ;;
+    esac
+done
---
+++ aws/switch_aws_profile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 執行此腳本請使用 source 指令
+
+# 取得執行腳本當前目錄
+DIR="$(dirname $0)"
+
+# 字體顏色
+source ${DIR}/modules/colors.sh
+
+set_profile() {
+  export AWS_PROFILE=${selected_account}
+  echo -e "${GREEN}Switched to AWS profile: $AWS_PROFILE${NC}"
+  aws sts get-caller-identity --no-cli-pager
+}
+
+accounts_list=($(aws configure list-profiles))
+
+current_account=$(env | grep AWS_PROFILE | cut -d'=' -f2)
+
+PS3="Select AWS Account: "
+select selected_account in "${accounts_list[@]}"; do
+    case $selected_account in
+      *)
+        if [[ -n "$selected_account" && "$selected_account" == "$current_account" ]]; then
+          echo -e "${YELLOW}Already using this account.${NC}"
+          echo -e "${BLUE}Logout and login again to switch account.${NC}"
+          unset AWS_PROFILE
+          set_profile
+        elif [ -n "$selected_account" ]; then
+          echo -e "${BLUE}Selected AWS Account: $selected_account${NC}"
+          set_profile
+        else
+          echo -e "${RED}Invalid selection.${NC}"
+        fi
+        break
+        ;;
+    esac
+done
