e738c6378d238b193a425a63e2cc0d12c761bef4
bing-wei <bing-wei@mail.chungyo.net>
2025-03-27 14:46:33 +0800

    feat: 更新 delete 腳本 & 新增 reset

--- kafka/reset_kafka_topics.sh
+++ kafka/reset_kafka_topics.sh
@@ -19,7 +19,7 @@ if [ -z "$BOOTSTRAP_SERVER" ]; then
 fi
 
 # 獲取所有 topic
-TOPICS=$($KAFKA_HOME/bin/kafka-topics.sh --list --bootstrap-server ${BOOTSTRAP_SERVER})
+TOPICS=$($KAFKA_HOME/bin/kafka-topics.sh --list --bootstrap-server $BOOTSTRAP_SERVER)
 
 # 檢查是否有任何 topic
 if [ -z "$TOPICS" ]; then
@@ -27,17 +27,28 @@ if [ -z "$TOPICS" ]; then
   exit 0
 fi
 
-echo "即將刪除以下 Kafka topics:"
+echo "即將刪除並重新創建以下 Kafka topics:"
 echo "$TOPICS"
 
-# 批次刪除所有 topic
+# 批次刪除並重新創建所有 topic
 for TOPIC in $TOPICS; do
-  $KAFKA_HOME/bin/kafka-topics.sh --delete --topic $TOPIC --bootstrap-server ${BOOTSTRAP_SERVER}
+  $KAFKA_HOME/bin/kafka-topics.sh --delete --topic $TOPIC --bootstrap-server $BOOTSTRAP_SERVER
 
   # 檢查刪除操作是否成功
   if [ $? -eq 0 ]; then
     echo "Kafka topic '$TOPIC' 已成功刪除。"
   else
     echo "刪除 Kafka topic '$TOPIC' 失敗。"
+    continue
+  fi
+
+  # 重新創建 topic 並設置分區數量為 1
+  $KAFKA_HOME/bin/kafka-topics.sh --create --topic $TOPIC --bootstrap-server $BOOTSTRAP_SERVER --partitions 1 --replication-factor 1
+
+  # 檢查創建操作是否成功
+  if [ $? -eq 0 ]; then
+    echo "Kafka topic '$TOPIC' 已成功重新創建，分區數量為 1。"
+  else
+    echo "重新創建 Kafka topic '$TOPIC' 失敗。"
   fi
 done
