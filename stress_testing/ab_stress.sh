93c760170e11021d47b9e8923cf0ddac4978957a
bing-wei <bing-wei@mail.chungyo.net>
2025-04-07 10:16:27 +0800

    feat: 移除原先自訂的網址

--- stress_testing/ab_stress.sh
+++ stress_testing/ab_stress.sh
@@ -7,12 +7,12 @@ source ../modules/default.sh
 read -p "是否使用預設值 (y/n): " use_default
 if [ "$use_default" == "y" ]; then
   echo -e "${GREEN}使用預設值...${NC}"
-  url=35.227.219.68/login
+  url=""
   protocol=https
   requests=50000
   concurrency=10
   time=600
-  host=admin.vir888.com
+  host=""
 else
   # 輸入需要測試的網址和請求數量
   read -p "請輸入需要測試的網址 (例如 example.com/path): " url
