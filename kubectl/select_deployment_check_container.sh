ac7ba46bcfcc8d1bf9d276d8712575ea344136b5
bing-wei <bing-wei@mail.chungyo.net>
2025-04-09 09:47:45 +0800

    feat: 檢查 container 詳細資訊腳本
    1. 全體 pod 掃描
    
    2. 選擇 deployment 抽查

---
+++ kubectl/select_deployment_check_container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source ../modules/default.sh
+source ../modules/kubernetes_context_list.sh
+
+# 顯示當前 kubernetes 的 context
+CURRENT_CONTEXTS=$(kubectl config current-context)
+echo -e "${BLUE}Current Kubernetes contexts: $CURRENT_CONTEXTS${NC}"
+
+# 顯示當前 kubernetes 的 namespace
+CURRENT_NAMESPACE=$(kubectl config view --minify -o jsonpath='{..namespace}')
+echo -e "${BLUE}Current Kubernetes namespace: $CURRENT_NAMESPACE${NC}"
+
+# 使用 fzf 選擇 deployment
+DEPLOYMENT_NAME=$(kubectl get deployment -o name | fzf)
+if [ -z "$DEPLOYMENT_NAME" ]; then
+  echo -e "${RED}No deployment selected.${NC}"
+  exit 1
+fi
+echo -e "${BLUE}Selected deployment: $DEPLOYMENT_NAME${NC}"
+
+# 查看 deployment 的 container name
+CONTAINER_NAME=$(kubectl get $DEPLOYMENT_NAME -o jsonpath='{.spec.template.spec.containers[*].name}')
+echo -e "${GREEN}Container Name: $CONTAINER_NAME${NC}"
+# 查看 deployment 的 container image
+CONTAINER_IMAGE=$(kubectl get $DEPLOYMENT_NAME -o jsonpath='{.spec.template.spec.containers[*].image}')
+echo -e "${GREEN}Container Image: $CONTAINER_IMAGE${NC}"
