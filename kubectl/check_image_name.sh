fc38091fc3fe87d7de7d7a64cc2f4de198d41440
bing-wei <bing-wei@mail.chungyo.net>
2025-04-15 16:35:43 +0800

    refactor: 優化 deployment 有複數的 container 可能

--- kubectl/check_image_name.sh
+++ kubectl/check_image_name.sh
@@ -2,6 +2,9 @@
 source ../modules/default.sh
 source ../modules/switch_kubernetes_context.sh
 
+# images 空陣列儲存所有的 image 名稱
+images=()
+
 # 檢查所有 deployments 內所有的 container 使用的 image 名稱
 # 如果有使用的 image 名稱開頭是 gcr.io 則顯示該 container 的資訊
 
@@ -12,9 +15,13 @@ if [ -z "$DEPLOYMENTS" ]; then
 fi
 
 for DEPLOYMENT in $DEPLOYMENTS; do
-  CONTAINER_IMAGE=$(kubectl get deployment $DEPLOYMENT --context $CURRENT_CONTEXT -n $CURRENT_NAMESPACE -o jsonpath='{.spec.template.spec.containers[*].image}')
-  if [[ "$CONTAINER_IMAGE" == gcr.io* ]]; then
-    echo -e "${BLUE}Selected deployment: $DEPLOYMENT${NC}"
-    echo -e "${GREEN}Container Image: $CONTAINER_IMAGE${NC}"
-  fi
+  CONTAINER_IMAGES=$(kubectl get deployment $DEPLOYMENT --context $CURRENT_CONTEXT -n $CURRENT_NAMESPACE -o jsonpath='{.spec.template.spec.containers[*].image}')
+  for CONTAINER_IMAGE in $CONTAINER_IMAGES; do
+    if [[ "$CONTAINER_IMAGE" == gcr.io/rd6-project/* ]]; then
+      echo -e "${BLUE}Selected deployment: $DEPLOYMENT${NC}"
+      echo -e "${GREEN}Container Image: $CONTAINER_IMAGE${NC}"
+      # 將 image 名稱加入 images 陣列
+      images+=("$CONTAINER_IMAGE")
+    fi
+  done
 done
