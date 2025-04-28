e4f9099e6e8638eb6e1282bbff0dd02ec742903d
bing-wei <bing-wei@mail.chungyo.net>
2025-04-28 09:54:30 +0800

    docs: 調整排版、註解

--- gcloud/iam_policy.sh
+++ gcloud/iam_policy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 source ../gcloud/switch_project.sh
 
-# 輸入要查詢的 serviceAccount
+# 輸入要查詢的 serviceAccount 關鍵字，查詢該 serviceAccount 的 IAM 權限
 read -p "Enter the service account (e.g. [SERVICE_ACCOUNT]@[PROJECT_ID].iam.gserviceaccount.com): " SERVICE_ACCOUNT
 
-# roles/iam
 echo -e "${BLUE}Service Account Permissions:${NC}"
-
 gcloud projects get-iam-policy $CURRENT_PROJECT --flatten="bindings[].members" --filter="bindings.members:$SERVICE_ACCOUNT" --format="table(bindings.role, bindings.members)"
