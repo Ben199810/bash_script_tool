4066c3d3d9e5737e15ed716b4a57c1edb9de4482
bing-wei <bing-wei@mail.chungyo.net>
2025-04-30 17:20:51 +0800

    fix: service-account 權限查詢腳本

--- gcloud/service-account_permissions.sh
+++ gcloud/service-account_permissions.sh
@@ -2,17 +2,17 @@
 source ../gcloud/switch_project.sh
 
 # 列出所有 service accounts 並使用 fzf 選擇
-service_account=$(gcloud iam service-accounts list --format="value(email)" | fzf)
-if [ -z "$service_account" ]; then
+SERVICE_ACCOUNT=$(gcloud iam service-accounts list --format="value(email)" | fzf)
+if [ -z "$SERVICE_ACCOUNT" ]; then
   echo -e "${RED}No service account selected.${NC}"
   exit 1
 fi
 
 # 查詢選定的 service account 的權限
-echo "Selected Service Account: $service_account"
+echo "Selected Service Account: $SERVICE_ACCOUNT"
 # roles/iam
 echo -e "${BLUE}Service Account Permissions:${NC}"
-gcloud projects get-iam-policy $current_project --flatten="bindings[].members" --format="table(bindings.role)" --filter="bindings.members:$service_account"
+gcloud projects get-iam-policy $CURRENT_PROJECT --flatten="bindings[].members" --format="table(bindings.role)" --filter="bindings.members:$SERVICE_ACCOUNT"
 # workload identity
 echo -e "${BLUE}Service Account Workload Identity:${NC}"
-gcloud iam service-accounts get-iam-policy $service_account
+gcloud iam service-accounts get-iam-policy $SERVICE_ACCOUNT
