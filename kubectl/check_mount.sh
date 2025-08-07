85a51f516f841e3baec2a6252aac901900bee6bd
bing-wei <bing-wei@mail.chungyo.net>
2025-08-07 15:31:30 +0800

    Adds script to check file system usage
    Adds a new script to check file system usage within Kubernetes pods.
    
    The script defines constants for file system IPs in different environments (DEV/QA, Staging, Prod) and retrieves pod names for various services. It then uses `kubectl exec` and `df -h` to display file system usage information for each pod, filtering by the predefined IPs for the current Kubernetes context. The script also includes a function to switch Kubernetes context and namespace.

--- kubectl/check_mount.sh
+++ kubectl/check_mount.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
-source ./switch_kubernetes_context.sh
+source ../modules/switch_kubernetes_context.sh
+
+switch_context_interface # 切換 Kubernetes Context 和 Namespace 的介面流程
+
+# 定義常數
+
+# DEV/QA 環境
+readonly DEV_QA_FILESTORE_IP="172.18.0.106"
+readonly DEV_CONTEXT="gke_gcp-20220425-004_asia-east1-b_bbin-interface-dev"
+readonly QA_CONTEXT="gke_gcp-20220425-005_asia-east1-b_bbin-interface-qa"
+# STAGING 環境的檔案系統 IP
+readonly STAGING_TXT_FILESTORE_IP="172.18.2.50"
+readonly STAGING_AIO_TXT_FILESTORE_IP="172.18.2.186"
+readonly STAGING_AH_TXT_FILESTORE_IP="172.18.3.18"
+readonly STAGING_CONTEXT="gke_gcp-20240131-024_asia-east1_bbin-interface-staging"
+# PROD 環境的檔案系統 IP
+readonly PROD_TXT_FILESTORE_IP="172.18.2.130"
+readonly PROD_AIO_TXT_FILESTORE_IP="172.18.2.66"
+readonly PROD_AH_TXT_FILESTORE_IP="172.18.2.194"
+readonly PROD_CONTEXT="gke_gcp-20220425-006_asia-east1_bbin-interface-prod"
+
+
 
 # 對 Pod 使用客製化的 df 指令，獲取想要的資訊
 pod_df() {
-  SEARCH_KEYWORD="$1"
+  PODS="$1"
   CONTAINER_NAME="$2"
 
-  DEV_QA_FILESTORE_IP="172.18.0.106"
-
-  STAGING_TXT_FILESTORE_IP="172.18.2.50"
-  STAGING_AIO_TXT_FILESTORE_IP="172.18.2.186"
-  STAGING_AH_TXT_FILESTORE_IP="172.18.3.18"
-
-  PROD_TXT_FILESTORE_IP="172.18.2.130"
-  PROD_AIO_TXT_FILESTORE_IP="172.18.2.66"
-  PROD_AH_TXT_FILESTORE_IP="172.18.2.194"
-
-  PODS=$(kubectl get pod --context="$CURRENT_CONTEXT" -n "$CURRENT_NAMESPACE" -o 'custom-columns=NAME:.metadata.name' | grep $SEARCH_KEYWORD)
-
-  # 檢查是否有找到符合條件的 Pod，如果沒有則退出
+  # 檢查是否有找到符合條件的 Pod，如果沒有找到，則輸出提示信息並返回錯誤碼
   if [ -z "$PODS" ]; then
-    echo "No pods found with keyword: $SEARCH_KEYWORD"
-    exit 0
+    echo -e "${YELLOW}無法找到符合搜尋條件的 Pod 在命名空間 $CURRENT_NAMESPACE 中${NC}"
+    return 1
   fi
 
   for POD in $PODS; do
     echo -e "${BLUE}Checking pod: $POD${NC}"
     # 檢查當前上下文是否為指定的環境
-    if [[ $CURRENT_CONTEXT == "gke_gcp-20220425-005_asia-east1-b_bbin-interface-qa" || $CURRENT_CONTEXT == "gke_gcp-20220425-004_asia-east1-b_bbin-interface-dev" ]]; then
+    if [[ $CURRENT_CONTEXT == "$QA_CONTEXT" || $CURRENT_CONTEXT == "$DEV_CONTEXT" ]]; then
       kubectl exec --context="$CURRENT_CONTEXT" -n "$CURRENT_NAMESPACE" "$POD" -c "$CONTAINER_NAME" -- df -h | awk '
       /^'"${DEV_QA_FILESTORE_IP}"'/ {
         filesystem = $0
         getline
         print "Filesystem:", filesystem, "Used:", $2, "Available:", $3, "Use%:", $4, "Mountpoint:", $5
       }'
-    elif [[ $CURRENT_CONTEXT == "gke_gcp-20240131-024_asia-east1_bbin-interface-staging" ]]; then
+    elif [[ $CURRENT_CONTEXT == "$STAGING_CONTEXT" ]]; then
       kubectl exec --context="$CURRENT_CONTEXT" -n "$CURRENT_NAMESPACE" "$POD" -c "$CONTAINER_NAME" -- df -h | awk '
       /^'"${STAGING_TXT_FILESTORE_IP}"'/ {
         filesystem = $0
@@ -53,7 +62,7 @@ pod_df() {
         getline
         print "Filesystem:", filesystem, "Used:", $2, "Available:", $3, "Use%:", $4, "Mountpoint:", $5
       }'
-    elif [[ $CURRENT_CONTEXT == "gke_gcp-20220425-006_asia-east1_bbin-interface-prod" ]]; then
+    elif [[ $CURRENT_CONTEXT == "$PROD_CONTEXT" ]]; then
       kubectl exec --context="$CURRENT_CONTEXT" -n "$CURRENT_NAMESPACE" "$POD" -c "$CONTAINER_NAME" -- df -h | awk '
       /^'"${PROD_TXT_FILESTORE_IP}"'/ {
         filesystem = $0
@@ -79,19 +88,17 @@ pod_df() {
 }
 
 # 取得 deployment 複數的 Pod 名稱，隨機選擇一個 Pod
-AIO_WEB_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep aio-web-ball-member | grep -v nginx-proxy | shuf -n 1)
-AIO_API_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep aio-api-ball-member | grep -v nginx-proxy | shuf -n 1)
-BALL_MEMBER_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep ball-member | grep -v nginx-proxy | grep -v aio | grep -v bg | shuf -n 1)
-BAMBI_OFFERCENTER_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep bambi-offercenter | shuf -n 1)
-INTERNAL_BLISSEY_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep internal-blissey | shuf -n 1)
-CTL_BLISSEY_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep ctl-blissey | shuf -n 1)
-HALL_BLISSEY_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep hall-blissey | shuf -n 1)
-EAGLE_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep eagle | shuf -n 1)
-WOLF_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep wolf | shuf -n 1)
-IPL_CTL_BACKGROUND_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep ipl-ctl-background | shuf -n 1)
-CHECK_INFO_POD=$(kubectl get pod -n interface --no-headers -o 'custom-columns=NAME:.metadata.name' | grep checkinfo | shuf -n 1)
-
-
+AIO_WEB_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep aio-web-ball-member | grep -v nginx-proxy | shuf -n 1)
+AIO_API_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep aio-api-ball-member | grep -v nginx-proxy | shuf -n 1)
+BALL_MEMBER_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep ball-member | grep -v nginx-proxy | grep -v aio | grep -v bg | shuf -n 1)
+BAMBI_OFFERCENTER_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep bambi-offercenter | shuf -n 1)
+INTERNAL_BLISSEY_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep internal-blissey | shuf -n 1)
+CTL_BLISSEY_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep ctl-blissey | shuf -n 1)
+HALL_BLISSEY_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep hall-blissey | shuf -n 1)
+EAGLE_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep eagle | shuf -n 1)
+WOLF_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep wolf | shuf -n 1)
+IPL_CTL_BACKGROUND_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep ipl-ctl-background | shuf -n 1)
+CHECK_INFO_POD=$(kubectl get pod -n $CURRENT_NAMESPACE --no-headers -o 'custom-columns=NAME:.metadata.name' | grep checkinfo | shuf -n 1)
 
 # 檢查 Pod 的檔案系統使用情況
 # read-write
