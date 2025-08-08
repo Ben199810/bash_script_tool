2caefa34929723912780d7f0b6d5838f32a87e7e
bing-wei <bing-wei@mail.chungyo.net>
2025-08-08 17:32:30 +0800

    feat: 改善 func 名稱

--- modules/switch_kubernetes_context.sh
+++ modules/switch_kubernetes_context.sh
@@ -4,13 +4,13 @@ source ../modules/default.sh
 declare CURRENT_CONTEXT=""
 declare CURRENT_NAMESPACE=""
 
-function current_context() {
+function get_current_context() {
   CURRENT_CONTEXT=$(kubectl config current-context)
   echo -e "${BLUE}當前本機環境 Kubernetes contexts: $CURRENT_CONTEXT${NC}"
 }
 
-function current_namespace() {
-  CURRENT_NAMESPACE=$(kubectl config view --minify -o jsonpath='{..namespace}')
+function get_current_namespace() {
+  CURRENT_NAMESPACE=$(kubectl config view --minify -o json | jq -r '.contexts[].context.namespace')
   echo -e "${BLUE}當前本機環境 Kubernetes namespace: $CURRENT_NAMESPACE${NC}"
 }
 
@@ -38,8 +38,8 @@ function switch_namespace() {
 
 # 實作切換 Kubernetes Context 和 Namespace 的介面流程
 switch_context_interface(){
-  current_context
-  current_namespace
+  get_current_context
+  get_current_namespace
 
   read -p "你想要切換 Kubernetes Context 嗎? (y/n): " SWITCH_CONTEXT
   if [[ "$SWITCH_CONTEXT" =~ ^[Yy]$ ]]; then
@@ -51,6 +51,6 @@ switch_context_interface(){
     echo -e "${YELLOW}跳過切換。${NC}"
   fi
 
-  current_context
-  current_namespace
+  get_current_context
+  get_current_namespace
 }
