9869caba94df23f26d96f802b957f8f8b9e7cbb4
Ben199810 <ben29774583@gmail.com>
2025-10-03 10:58:32 +0800

    update: 優化程式碼行數，read 提示訊息不使用顏色輸出

--- modules/default.sh
+++ modules/default.sh
@@ -9,19 +9,13 @@ NC='\033[0m' # No Color
 
 # 全域變數
 SEARCH_KEYWORD=""
-# 函數：請使用者輸入要搜尋的關鍵字
+# 請使用者輸入要搜尋的關鍵字
 function prompts_search_keyword() {
-    while [[ -z "$SEARCH_KEYWORD" ]]; do
-        echo -en "${YELLOW}請輸入要搜尋的關鍵字: ${NC}"
-        read -r SEARCH_KEYWORD
-        
-        if [[ -z "$SEARCH_KEYWORD" ]]; then
-            echo -e "${RED}❌ 關鍵字不能為空，請重新輸入${NC}"
-            echo ""
-        fi
-    done
-    
-    echo ""
-    echo -e "${GREEN}✅ 搜尋關鍵字: $SEARCH_KEYWORD${NC}"
-    echo ""
+  while [[ -z "$SEARCH_KEYWORD" ]]; do
+    read -rp "請輸入要搜尋的關鍵字: " SEARCH_KEYWORD
+    if [[ -z "$SEARCH_KEYWORD" ]]; then
+      echo -e "${RED}❌ 關鍵字不能為空，請重新輸入${NC}"
+    fi
+  done
+  echo -e "${GREEN}✅ 搜尋關鍵字: $SEARCH_KEYWORD${NC}"
 }
